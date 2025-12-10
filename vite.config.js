4811645137824041ee4346c8a0f81b32dae0d262
AJ Spicknall <arnoldspicknall@weber.edu>
2025-12-10 10:35:10 -0700

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-const path = require("path")
-const glob = require("glob")
-
+import path from "path"
+import glob from "glob"
+import { fileURLToPath } from "url"
 import { defineConfig } from "vite"
 import eslint from "@rollup/plugin-eslint"
 
-let input = {}
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const input = {}
 
 glob
   .sync("./src/**/*.html")
-  .map((file) => {
-    return [file.substring(`.${path.sep}src${path.sep}`.length), file]
-  })
-  .forEach((f) => {
-    input[f[0]] = f[1]
+  .map((file) => [file.substring(`.${path.sep}src${path.sep}`.length), file])
+  .forEach(([key, value]) => {
+    input[key] = value
   })
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   root: "src",
   build: {
     manifest: true,
@@ -23,19 +24,23 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input
-    },
+    }
   },
   server: {
     port: 8080,
-    open: "/",
+    open: "/"
   },
   plugins: [
-    {
-      ...eslint({
-        include: ["src/**/*.js"],
-      }),
-      enforce: "pre",
-      apply: "build",
-    },
-  ],
-})
+    mode !== "production" &&
+      {
+        ...eslint({
+          include: ["src/**/*.js"],
+          configFile: path.resolve(__dirname, ".eslintrc.json"),
+          throwOnError: false,
+          throwOnWarning: false
+        }),
+        enforce: "pre",
+        apply: "serve"
+      }
+  ].filter(Boolean)
+}))
